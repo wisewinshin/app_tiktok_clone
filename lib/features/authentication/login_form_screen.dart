ae55c783cbbc9f12d2479ea8c0a7d1e75e883d19
wisewinshin <dsds666a@naver.com>
2023-02-08 17:28:34 +0900

    create login form screen

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -1,10 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/onboarding/interest_screen.dart';
 
-class LoginFormScreen extends StatelessWidget {
+class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
 
   @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  final Map<String, String> _formData = {};
+
+  void _onSubmitTap() {
+    if (_formkey.currentState != null && _formkey.currentState!.validate()) {
+      _formkey.currentState!.save();
+      Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const InterestingScreen(),
+      ),
+    );
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Log in"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size36,
+        ),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              Gaps.v28,
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Email",
+                ),
+                validator: (value) {
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    _formData["email"] = newValue;
+                  }
+                },
+              ),
+              Gaps.v16,
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Password",
+                ),
+                validator: (value) {
+                  return null;
+                },
+                onSaved: (newValue) {
+                  if (newValue != null) {
+                    _formData["password"] = newValue;
+                  }
+                },
+              ),
+              Gaps.v28,
+              GestureDetector(
+                onTap: _onSubmitTap,
+                child: const FormButton(
+                  disabled: false,
+                  text: "Log in",
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
