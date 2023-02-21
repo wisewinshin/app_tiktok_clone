d7649066b4ca7f498878ccc047fb72d5fa0ffbaa
wisewinshin <92836658+wisewinshin@users.noreply.github.com>
2023-02-22 00:11:45 +0900

    change placeholder image

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -65,7 +65,7 @@ class DiscoverScreen extends StatelessWidget {
                     child: FadeInImage.assetNetwork(
                       fit: BoxFit.cover,
                       placeholderFit: BoxFit.cover,
-                      placeholder: "assets/images/placeholder.png",
+                      placeholder: "assets/images/placeholder.jpg",
                       image:
                           "https://images.unsplash.com/photo-1619650277752-9b853abf815b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3840&q=80",
                     ),
