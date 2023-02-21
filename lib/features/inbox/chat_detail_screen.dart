a5573dee93254a739a2928c2687c63fac778a922
wisewinshin <dsds666a@naver.com>
2023-02-21 18:02:47 +0900

    editing chatdetailscreen ...

--- lib/features/inbox/chat_detail_screen.dart
+++ lib/features/inbox/chat_detail_screen.dart
@@ -1,10 +1,145 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class ChatDetailScreen extends StatelessWidget {
   const ChatDetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: ListTile(
+          contentPadding: EdgeInsets.zero,
+          horizontalTitleGap: Sizes.size8,
+          leading: const CircleAvatar(
+            foregroundImage: NetworkImage(
+                "https://d1telmomo28umc.cloudfront.net/media/public/avatars/wisewinshin-avatar.jpg"),
+            radius: Sizes.size24,
+          ),
+          title: const Text(
+            "현승",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: const Text("Active Now"),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              FaIcon(
+                FontAwesomeIcons.flag,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+              Gaps.h32,
+              FaIcon(
+                FontAwesomeIcons.ellipsis,
+                color: Colors.black,
+                size: Sizes.size20,
+              )
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            child: ListView.separated(
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size20,
+                  horizontal: Sizes.size14,
+                ),
+                itemBuilder: (context, index) {
+                  final isMine = index % 2 == 0;
+                  return Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: isMine
+                        ? MainAxisAlignment.end
+                        : MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(Sizes.size14),
+                        decoration: BoxDecoration(
+                          color: !isMine
+                              ? Theme.of(context).primaryColor
+                              : Colors.blue,
+                          borderRadius: BorderRadius.only(
+                            topLeft: const Radius.circular(
+                              Sizes.size12,
+                            ),
+                            topRight: const Radius.circular(
+                              Sizes.size12,
+                            ),
+                            bottomLeft: isMine
+                                ? const Radius.circular(
+                                    Sizes.size12,
+                                  )
+                                : Radius.zero,
+                            bottomRight: isMine
+                                ? Radius.zero
+                                : const Radius.circular(
+                                    Sizes.size12,
+                                  ),
+                          ),
+                        ),
+                        child: const Text(
+                          "Message",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: Sizes.size16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+                separatorBuilder: (context, index) => Gaps.v10,
+                itemCount: 12),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: BottomAppBar(
+              color: Colors.grey.shade50,
+              padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size8, horizontal: Sizes.size16),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: "Send a message...",
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey.shade400,
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey.shade400,
+                          ),
+                        ),
+                      ),
+                      cursorColor: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  Gaps.h10,
+                  const FaIcon(
+                    FontAwesomeIcons.paperPlane,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
