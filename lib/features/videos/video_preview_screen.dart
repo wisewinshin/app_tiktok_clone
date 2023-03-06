a1524bda51e175c8499a2754635dc1022cb86b79
wisewin <92836658+wisewinshin@users.noreply.github.com>
2023-03-06 16:17:49 +0900

    create videopreview, recording screen

---
+++ lib/features/videos/video_preview_screen.dart
@@ -0,0 +1,78 @@
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPreviewScreen extends StatefulWidget {
+  final bool isPicked;
+  final XFile videos;
+  const VideoPreviewScreen({
+    super.key,
+    required this.videos,
+    required this.isPicked,
+  });
+
+  @override
+  State<VideoPreviewScreen> createState() => _VideoPreviewScreenState();
+}
+
+class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
+  late final VideoPlayerController _videoPlayerController;
+
+  Future<void> _initVideo() async {
+    _videoPlayerController =
+        VideoPlayerController.file(File(widget.videos.path));
+    await _videoPlayerController.initialize();
+    await _videoPlayerController.setLooping(true);
+    await _videoPlayerController.play();
+    setState(() {});
+  }
+
+  Future<void> _saveVideo() async {
+    await GallerySaver.saveVideo(widget.videos.path);
+  }
+
+  Future<void> _initPermission() async {
+    final PermissionStatus storagePermission =
+        await Permission.storage.request();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initPermission();
+    _initVideo();
+  }
+
+
+  @override
+  void dispose() {
+    _videoPlayerController.pause();
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("미리보기"),
+        actions: [
+          if (!widget.isPicked)
+            IconButton(
+              onPressed: _saveVideo,
+              icon: const Icon(
+                FontAwesomeIcons.floppyDisk,
+              ),
+            )
+        ],
+      ),
+      body: _videoPlayerController.value.isInitialized
+          ? VideoPlayer(_videoPlayerController)
+          : null,
+    );
+  }
+}
---
+++ lib/features/videos/video_recording_screen.dart
@@ -0,0 +1,303 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/videos/video_preview_screen.dart';
+
+class VideoRecordingScreen extends StatefulWidget {
+  const VideoRecordingScreen({super.key});
+
+  @override
+  State<VideoRecordingScreen> createState() => _VideoRecordingScreenState();
+}
+
+class _VideoRecordingScreenState extends State<VideoRecordingScreen>
+    with TickerProviderStateMixin, WidgetsBindingObserver {
+  late CameraController _cameraController;
+  bool _hasPermissons = false;
+  bool _isSelfieMode = false;
+  late FlashMode _flashMode;
+
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(
+      milliseconds: 200,
+    ),
+  );
+  late final Animation<double> _buttonAnimation =
+      Tween(begin: 1.0, end: 1.1).animate(_animationController);
+
+  late final AnimationController _progressAnimationController =
+      AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 3),
+    lowerBound: 0.0,
+    upperBound: 1.0,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    initPermissions();
+    initCamera();
+    _progressAnimationController.addListener(() {
+      setState(() {});
+    });
+    _progressAnimationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _stopRecording();
+      }
+    });
+  }
+
+  Future<void> initPermissions() async {
+    final PermissionStatus cameraPermission = await Permission.camera.request();
+    final PermissionStatus micPermission =
+        await Permission.microphone.request();
+
+    final bool cameraDenied =
+        cameraPermission.isDenied || cameraPermission.isPermanentlyDenied;
+    final bool micDenied =
+        micPermission.isDenied || micPermission.isPermanentlyDenied;
+
+    if (!cameraDenied && !micDenied) {
+      setState(() {
+        _hasPermissons = true;
+      });
+    }
+  }
+
+  Future<void> initCamera() async {
+    final cameras = await availableCameras();
+    if (cameras.isEmpty) {
+      return;
+    }
+    _cameraController = CameraController(
+      cameras[_isSelfieMode ? 1 : 0],
+      ResolutionPreset.high,
+    );
+
+    await _cameraController.initialize();
+    await _cameraController.prepareForVideoRecording(); //애플전용
+    _flashMode = _cameraController.value.flashMode;
+    setState(() {});
+  }
+
+  Future<void> _toggleSelfieMode() async {
+    _isSelfieMode = !_isSelfieMode;
+    await initCamera();
+  }
+
+  Future<void> _rotateflashMode() async {
+    switch (_flashMode) {
+      case FlashMode.auto:
+        _flashMode = FlashMode.off;
+        break;
+      case FlashMode.off:
+        _flashMode = FlashMode.always;
+        break;
+      case FlashMode.always:
+        _flashMode = FlashMode.torch;
+        break;
+      case FlashMode.torch:
+        _flashMode = FlashMode.auto;
+        break;
+      default:
+        _flashMode = FlashMode.auto;
+    }
+    await _cameraController.setFlashMode(_flashMode);
+    setState(() {});
+  }
+
+  IconData getFlashIcon() {
+    switch (_flashMode) {
+      case FlashMode.always:
+        return Icons.flash_on_rounded;
+      case FlashMode.auto:
+        return Icons.flash_auto_rounded;
+      case FlashMode.off:
+        return Icons.flash_off_rounded;
+      case FlashMode.torch:
+        return Icons.flashlight_on;
+      default:
+        return Icons.flash_auto_rounded;
+    }
+  }
+
+  Future<void> _startRecording(TapDownDetails details) async {
+    if (_cameraController.value.isRecordingVideo) {
+      return;
+    }
+    await _cameraController.startVideoRecording();
+    _animationController.forward();
+    _progressAnimationController.forward();
+  }
+
+  Future<void> _stopRecording() async {
+    if (!_cameraController.value.isRecordingVideo) {
+      return;
+    }
+    _animationController.reverse();
+    _progressAnimationController.reset();
+    final XFile file = await _cameraController.stopVideoRecording();
+
+    if (!mounted) return;
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => VideoPreviewScreen(
+          videos: file,
+          isPicked: false,
+        ),
+      ),
+    );
+  }
+
+  Future<void> _onPickVideosPressed() async {
+    final video = await ImagePicker().pickVideo(source: ImageSource.gallery);
+    if (video == null) return;
+    if (!mounted) return;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => VideoPreviewScreen(
+          videos: video,
+          isPicked: true,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Future<void> didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (!_cameraController.value.isInitialized) return;
+    if (state == AppLifecycleState.inactive) {
+      _cameraController.dispose();
+    } else if (state == AppLifecycleState.resumed) {
+      await initCamera();
+    }
+    super.didChangeAppLifecycleState(state);
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    _animationController.dispose();
+    _progressAnimationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _hasPermissons && _cameraController.value.isInitialized
+            ? Stack(
+                alignment: Alignment.center,
+                children: [
+                  CameraPreview(_cameraController),
+                  Positioned(
+                    left: Sizes.size20,
+                    top: Sizes.size20,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          onPressed: _cameraController.value.isInitialized
+                              ? _toggleSelfieMode
+                              : null,
+                          icon: const Icon(
+                            Icons.cameraswitch,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Gaps.v12,
+                        IconButton(
+                          onPressed: _rotateflashMode,
+                          icon: Icon(
+                            _cameraController.value.isInitialized
+                                ? getFlashIcon()
+                                : null,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    width: MediaQuery.of(context).size.width,
+                    bottom: Sizes.size32,
+                    child: Row(
+                      children: [
+                        const Spacer(),
+                        GestureDetector(
+                          onTapDown: _startRecording,
+                          onTapUp: (detail) => _stopRecording(),
+                          child: ScaleTransition(
+                            scale: _buttonAnimation,
+                            child: Container(
+                              padding: const EdgeInsets.all(Sizes.size4),
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                              ),
+                              child: Stack(
+                                clipBehavior: Clip.none,
+                                alignment: Alignment.center,
+                                children: [
+                                  Positioned(
+                                    width: Sizes.size60 + Sizes.size6,
+                                    height: Sizes.size60 + Sizes.size6,
+                                    child: CircularProgressIndicator(
+                                      value: _progressAnimationController.value,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                  Container(
+                                    width: Sizes.size60,
+                                    height: Sizes.size60,
+                                    decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            alignment: Alignment.center,
+                            child: IconButton(
+                              onPressed: _onPickVideosPressed,
+                              icon: const Icon(
+                                FontAwesomeIcons.image,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            : Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text("권한주세요"),
+                  Gaps.v10,
+                  CircularProgressIndicator.adaptive(),
+                ],
+              ),
+      ),
+    );
+  }
+}
